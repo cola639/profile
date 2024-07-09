8f6c496f3b13d885e572e48f3a46fd2bd11fd09d
cola639 <337507950@qq.com>
2024-07-09 10:34:37 +0800

    update: 更新读取语言

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 // 判断当前url是否包含cn
 import {paramToObj} from "./utils";
 const {lang} = paramToObj();
-const isEn = lang == "en";
+const isEn = lang === "en";
 // isEn是true 导入portfolio_EN
 const language = isEn ? require("./portfolio_EN") : require("./portfolio_CN");
 // 读取里面导出的变量
